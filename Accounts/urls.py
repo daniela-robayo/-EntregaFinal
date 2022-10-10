1aece07d1bfcdab0a6a64541a05f55ec0c893d12
DanielaRobayo <danielaaarobayo@gmail.com>
2022-10-09 21:50:22 -0500

    URL correcto para SignUp

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -5,7 +5,7 @@ from BlogPage.views import *
 
 urlpatterns = [
     path('', home),
-    path('create_estudiantes/',create_estudiantes),
+    path('signup/',create_estudiantes),
     path('read_estudiantes/',read_estudiantes),
     path('update_estudiantes/<estudiante_email>',update_estudiantes),
     path('delete_estudiantes/<estudiante_email>',delete_estudiantes),
